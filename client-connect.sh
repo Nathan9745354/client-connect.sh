1aafe9d329ffac7fa6bd4ec03033aa1f3d5cbb34
Nathan9745354 <46953407+Nathan9745354@users.noreply.github.com>
2022-06-27 02:57:19 +0800

    Update client-connect.sh

--- client-connect.sh
+++ client-connect.sh
@@ -1,4 +1,4 @@
-sudo su
+#!/bin/sh
 
 echo "script-security 2" >> /etc/openvpn/server/server.conf
 echo "client-connect /var/log/script.sh" >> /etc/openvpn/server/server.conf
@@ -14,4 +14,11 @@ local_ip=$ifconfig_pool_remote_ip
 echo $(date) $user $remote_ip $local_ip >>/var/log/logfile.log
 " >> /var/log/script.sh
 
-echo" " >>/var/log/logfile.log
+echo " " >>/var/log/logfile.log
+
+chmod 777 /var/log/script.sh
+chmod 777 /var/log/logfile.log
+
+systemctl restart openvpn-server@server.service
+
+exit
