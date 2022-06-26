1c8caaa6b18cebd6ce7eb35435d7937cfc6a9c63
Nathan9745354 <46953407+Nathan9745354@users.noreply.github.com>
2022-06-27 02:53:21 +0800

    Create client-connect.sh

---
+++ client-connect.sh
@@ -0,0 +1,17 @@
+sudo su
+
+echo "script-security 2" >> /etc/openvpn/server/server.conf
+echo "client-connect /var/log/script.sh" >> /etc/openvpn/server/server.conf
+
+
+echo "
+#!/bin/sh
+
+user=$common_name
+remote_ip=$trusted_ip
+local_ip=$ifconfig_pool_remote_ip
+
+echo $(date) $user $remote_ip $local_ip >>/var/log/logfile.log
+" >> /var/log/script.sh
+
+echo" " >>/var/log/logfile.log
