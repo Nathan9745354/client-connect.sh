e195b655225db7933b7346a2bf148d3597decbbf
Nathan9745354 <46953407+Nathan9745354@users.noreply.github.com>
2022-06-27 03:04:54 +0800

    Update ovpnclient.sh

--- ovpnclient.sh
+++ ovpnclient.sh
@@ -4,7 +4,8 @@ echo "script-security 2" >> /etc/openvpn/server/server.conf
 echo "client-connect /var/log/script.sh" >> /etc/openvpn/server/server.conf
 
 
-echo "
+echo 
+"
 #!/bin/sh
 user=$common_name
 remote_ip=$trusted_ip
@@ -12,7 +13,7 @@ local_ip=$ifconfig_pool_remote_ip
 echo $(date) $user $remote_ip $local_ip >>/var/log/logfile.log
 " >> /var/log/script.sh
 
-echo " " >>/var/log/logfile.log
+echo " " >> /var/log/logfile.log
 
 chmod 777 /var/log/script.sh
 chmod 777 /var/log/logfile.log
