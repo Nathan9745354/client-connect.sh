6c06d0a06f9745b1b8368fc115fed4e4583dfc7d
Nathan9745354 <46953407+Nathan9745354@users.noreply.github.com>
2022-06-27 10:02:41 +0800

    Update buclilog.sh

--- buclilog.sh
+++ buclilog.sh
@@ -3,16 +3,12 @@ sudo su
 echo "script-security 2" >> /etc/openvpn/server/server.conf
 echo "client-connect /var/log/script.sh" >> /etc/openvpn/server/server.conf
 
-touch /var/log/script.sh
-
-nano /var/log/script.sh
-
 
 cd /var/log
 
-wget https://raw.githubusercontent.com/Nathan9745354/client-connect.sh/main/logfile.log
+wget https://raw.githubusercontent.com/Nathan9745354/client-connect.sh/main/script.sh
 
-echo " " >> /var/log/logfile.log
+touch /var/log/logfile.log
 
 chmod 777 /var/log/script.sh
 chmod 777 /var/log/logfile.log
